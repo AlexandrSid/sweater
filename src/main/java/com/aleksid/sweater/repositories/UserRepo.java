56b048eac06486e1f59a2d2b54a25829caf0d14e
alexa <alexandr.sidorkin@gmail.com>
2019-07-25 00:56:12 +0300

    Hibernate. Messaged become autorized.

---
+++ src/main/java/com/aleksid/sweater/repositories/UserRepo.java
@@ -0,0 +1,8 @@
+package com.aleksid.sweater.repositories;
+
+import com.aleksid.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
